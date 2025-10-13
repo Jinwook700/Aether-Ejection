11ccf56a5ca9938dd46205512f4c6d228a9751e0
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-10-13 22:22:28 +0900

    임시저장

--- Aether Ejection/Assets/Scripts/Player.cs
+++ Aether Ejection/Assets/Scripts/Player.cs
@@ -5,30 +5,58 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpScale = 7f;
+    public float jumpScale = 1f;
 
-    public Rigidbody2D rb;
+    [Header("Ground Check")]
+    public float checkRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    private bool isGrounded;
+    private bool jumpRequested = false;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
     private void Update()
     {
-        HorizontalMove();
-        JumpMove();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpRequested = true;
+        }
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 groundCheckPos = boxCollider.bounds.center + Vector3.down * (boxCollider.bounds.extents.y - 0.05f);
+        isGrounded = Physics2D.OverlapCircle(groundCheckPos, checkRadius, whatIsGround);
+
+        HorizontalMove();
+
+        if (jumpRequested && isGrounded)
+        {
+            JumpMove();
+            jumpRequested = false;
+        }
+        else if (jumpRequested)
+        {
+            jumpRequested = false;
+        }
+    }
+
     public void HorizontalMove()
     {
-        float horizontalDir = Input.GetAxis("Horizontal");
-
+        float horizontalDir = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontalDir * moveSpeed, rb.velocity.y);
     }
     public void JumpMove()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector3.up * jumpScale, ForceMode2D.Impulse);
-        }
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector3.up * jumpScale, ForceMode2D.Impulse);
     }
 }
 
