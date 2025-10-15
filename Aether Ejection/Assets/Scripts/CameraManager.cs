27204b9072202e60fe40e7a4a637218f53dca2ea
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-10-15 22:58:44 +0900

    Update Camera Manager : Player 따라오는 로직 생성

--- Aether Ejection/Assets/Scripts/CameraManager.cs
+++ Aether Ejection/Assets/Scripts/CameraManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float followThreshold = 7f;
+    [SerializeField] private float smoothSpeed = 5f;
+
+    private Transform player;
+
+    private void Start()
     {
-        
+        player = Player.Instance.transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        if (player == null) return;
+
+        Vector3 pos = transform.position;
+        Vector3 playerPos = player.position;
+
+        Vector3 offset = playerPos - pos;
+
+        if (Mathf.Abs(offset.x) > followThreshold)
+            pos.x += Mathf.Sign(offset.x) * (Mathf.Abs(offset.x) - followThreshold);
+
+        if (Mathf.Abs(offset.y) > followThreshold)
+            pos.y += Mathf.Sign(offset.y) * (Mathf.Abs(offset.y) - followThreshold);
+
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * smoothSpeed);
     }
 }
